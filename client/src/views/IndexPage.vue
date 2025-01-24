582d3ff2619bbb6d4c05a50d08b6755244322c1a
Lucas Legendre <lucas.lucasleg@gmail.com>
2025-01-24 01:24:04 +0100

    Responsive page Parcours
    mise au responsive de la page Parcours

--- client/src/views/IndexPage.vue
+++ client/src/views/IndexPage.vue
@@ -4,7 +4,7 @@
       <h1>Lucas <br>Legendre</h1>
       <div class="CarteVerte">
         <h2>Développeur web</h2>
-        <p> Je me présente Lucas Legendre, j’ai 27 ans et je suis développeur web, randonneurs, voyageur et un geek. <br> Je suis un développeur web passionné de création numérique, d’exploration et d’aventures. Curieux et toujours en quête de nouveaux défis, je mêle savoir-faire et esprit créatif. <br> Explorez mon site pour découvrir mes projets, mon parcours, ainsi que mes autres passions. Bienvenue dans mon univers !</p>
+        <p> Je me présente Lucas Legendre, j’ai 27 ans et je suis développeur web, randonneurs, voyageur et un geek. Je suis un développeur web passionné de création numérique, d’exploration et d’aventures. Curieux et toujours en quête de nouveaux défis, je mêle savoir-faire et esprit créatif. Explorez mon site pour découvrir mes projets, mon parcours, ainsi que mes autres passions. Bienvenue dans mon univers !</p>
       </div>
     </section>
     <section id="MonParcours">
--- client/src/views/MonParcours.vue
+++ client/src/views/MonParcours.vue
@@ -9,7 +9,7 @@
       <section id="SectionParcours">
         <h2>Mon <br> parcours</h2>
         <div class="CartesParcours">
-          <img src="" alt="Illustration du parcours">
+          <img src="" alt="Illustration du parcours" class="Orange">
           <div class="CorpsCartesParcours Vert">
             <h3>Titre de la période</h3>
             <div class="DatesCartesParcours">
@@ -29,7 +29,47 @@
           </div>
         </div>
         <div class="CartesParcours">
-          <img src="" alt="Illustration du parcours">
+          <img src="" alt="Illustration du parcours" class="Vert">
+          <div class="CorpsCartesParcours Orange">
+            <h3>Titre de la période</h3>
+            <div class="DatesCartesParcours">
+              <p>Nov 2015 - Dec 2018</p>
+              <div class="TexteCartesParcours">
+                <h4>Sous-titre</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              </div>
+            </div>
+            <div class="DatesCartesParcours">
+              <p>Avr 2020 - Jan 2022</p>
+              <div class="TexteCartesParcours">
+                <h4>Sous-titre</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="CartesParcours">
+          <img src="" alt="Illustration du parcours" class="Orange">
+          <div class="CorpsCartesParcours Vert">
+            <h3>Titre de la période</h3>
+            <div class="DatesCartesParcours">
+              <p>Nov 2015 - Dec 2018</p>
+              <div class="TexteCartesParcours">
+                <h4>Sous-titre</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              </div>
+            </div>
+            <div class="DatesCartesParcours">
+              <p>Avr 2020 - Jan 2022</p>
+              <div class="TexteCartesParcours">
+                <h4>Sous-titre</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="CartesParcours">
+          <img src="" alt="Illustration du parcours" class="Vert">
           <div class="CorpsCartesParcours Orange">
             <h3>Titre de la période</h3>
             <div class="DatesCartesParcours">
